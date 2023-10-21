74702c538ce9c01e611fc2dfa2bc8155330f1c8c
amikraa <129583293+amikraa@users.noreply.github.com>
2023-10-21 17:26:24 +0530

    Create config.py

---
+++ src/config.py
@@ -0,0 +1,11 @@
+# config.py
+
+# Telegram Bot Token
+TOKEN = "6325965135:AAGF9bvZwHqf3Hj2HcAdHcfvDzbHoZG_Eb0"
+
+
+# Other Configuration Options
+# For example, you can define API keys, database connection details, or any other settings here.
+
+# Example API Key
+# API_KEY = "YOUR_API_KEY"
